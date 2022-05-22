15cbc414b84f3c89f30f082c53f1fb309eedfb7a
Kirill Larchenkov <90748716+KirC0ff@users.noreply.github.com>
2022-05-22 20:03:58 +0300

    Create Token.java

---
+++ src/compiler/Token.java
@@ -0,0 +1,11 @@
+package compiler;
+
+public class Token {
+    private final String type;
+    private final String token;
+
+    public Token(String type, String token){
+        this.type = type;
+        this.token = token;
+    }
+}
